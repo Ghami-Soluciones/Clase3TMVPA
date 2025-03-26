af5a1e7c1494bae3e0cc1ab924d803ad6805aadc
Gamaliel Quiroz <ghamisoluciones@gmail.com>
2025-03-26 10:18:07 -0300

    Agrego saludo

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JOptionPane.showMessageDialog(null, "Hola");
+		
+		JOptionPane.showMessageDialog(null, "Espero que la esten pasando bien :D");
 	}
 }
